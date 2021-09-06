806ebecdf499169c3bf01f4a06606c20f7b53dbf
dangerpwnd <reric2012@gmail.com>
2021-09-05 23:39:51 -0500

    Create Location Pages

---
+++ src/pages/locations/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../../components/navigation.jsx';
+
+const Locations = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>List of Fantastic Locations</h1>
+        </div>
+    )
+}
+
+export default Locations;
