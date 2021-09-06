f778b7aebd1b0af1b7e150fb799b8ecb14bee0c2
dangerpwnd <reric2012@gmail.com>
2021-09-05 23:40:13 -0500

    Create Secrets & Clues Page

---
+++ src/pages/secrets/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../../components/navigation.jsx';
+
+const Secrets = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>Secrets & Clues</h1>
+        </div>
+    )
+}
+
+export default Secrets;
