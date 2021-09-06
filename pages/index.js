48e2ea3101b5f1339ec03e6cfdbe43850acbd91d
dangerpwnd <reric2012@gmail.com>
2021-09-05 21:45:02 -0500

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return(
+        <div>
+            Welcome to the Lazy DM Dashboard
+        </div>
+    )
+}
+
+export default HomePage
+
