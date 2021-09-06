326f8b9507fe33a36cef073560b988e769929077
dangerpwnd <reric2012@gmail.com>
2021-09-05 23:40:22 -0500

    Create Potential Scenes Page

---
+++ src/pages/scenes/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../../components/navigation.jsx';
+
+const Scenes = () => {
+    return (
+    <div>
+        <Navigation />
+        <h1>Possible Scenes</h1>
+    </div>
+    )
+
+}
+
+export default Scenes;
