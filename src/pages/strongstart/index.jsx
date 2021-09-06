355c63f80eaed0c831492a0fdba6b4f6f5f852d9
dangerpwnd <reric2012@gmail.com>
2021-09-05 23:40:02 -0500

    Create Strong Start Page

---
+++ src/pages/strongstart/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../../components/navigation.jsx'
+;
+
+const StrongStart = () => {
+    return(
+    <div>
+        <Navigation />
+        <h1>Create a Strong Start</h1>
+    </div>
+    )
+}
+
+export default StrongStart
