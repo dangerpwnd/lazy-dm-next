a3277bb30c6f6b1b948edd54e6d4c9ff385391b9
dangerpwnd <reric2012@gmail.com>
2021-09-05 23:38:24 -0500

    Add Theme-UI library

---
+++ theme.js
@@ -0,0 +1,19 @@
+import { roboto } from '@theme-ui/presets';
+
+const theme = {
+    ...roboto,
+    containers: {
+        card: {
+            p:2,
+        },
+        page: {
+            m: 0,
+            p: 1,
+        }
+    },
+    styles: {
+        ...roboto.styles
+    }
+}
+
+export default theme;
