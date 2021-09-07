585f092308de3d1937e614a9f45afbade57b0f81
dangerpwnd <reric2012@gmail.com>
2021-09-06 21:33:20 -0500

    Add NPCs, Monsters, MagicItems pages to Navigation component

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,18 +1,21 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import Link  from 'next/link';
+import { jsx } from "theme-ui";
+import Link from "next/link";
 
 const Navigation = () => {
-    return (
-        <nav sx={{bg:'primary', variant: 'containers.page'}}>
-            <Link href="/characters">Characters</Link>
-            <Link href="/strongstart">Strong Start</Link>
-            <Link href="/scenes">Possible Scenes</Link>
-            <Link href="/secrets">Secrets & Clues</Link>
-            <Link href="/locations">Fantastic Locations</Link>
-        </nav>
-    )
-}
+  return (
+    <nav sx={{ bg: "primary", variant: "containers.page" }}>
+      <Link href="/characters">Characters</Link>
+      <Link href="/strongstart">Strong Start</Link>
+      <Link href="/scenes">Possible Scenes</Link>
+      <Link href="/secrets">Secrets & Clues</Link>
+      <Link href="/locations">Fantastic Locations</Link>
+      <Link href="/npcs">Important NPCs</Link>
+      <Link href="/npcs">Relevant Monsters</Link>
+      <Link href="/npcs">Magic Items</Link>
+    </nav>
+  );
+};
 
 export default Navigation;
