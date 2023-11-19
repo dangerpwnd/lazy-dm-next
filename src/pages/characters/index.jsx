9151a5385e4a14aeba3917dc0d683c56b31f2cd5
Eric Dangerstone <reric2012@gmail.com>
2023-11-19 00:46:31 -0600

    Added initial layout/styles for characters page

--- src/pages/characters/index.jsx
+++ src/pages/characters/index.jsx
@@ -4,9 +4,85 @@ import Navigation from '../../components/navigation.jsx';
 
 const Characters = () => {
   return (
-    <div>
-      <Navigation />
-      <h1 class="font-serif">List of characters</h1>
+    <div className="flex flex-row gap-4">
+      <Navigation className="w-1/5" />
+      <section className="grid grid-auto-rows grid-cols-4 w-2/3 gap-4">
+        <h1 className="col-span-4">Characters</h1>
+        <div className="col-span-3">
+          <h2>Current Characters:</h2>
+          <div className="flex flex-col bg-stone-wall h-full row-span-4">
+            <div className="bg-gray-700 opacity-80 p-4 m-auto w-4/5 flex">
+              <ul className="w-2/3">
+                <li>Character Name</li>
+                <li>Character Class/Lvl</li>
+                <li>Character Race/Background</li>
+              </ul>
+              <ul className="w-1/3">
+                <li>Edit</li>
+                <li>Favorite</li>
+                <li>Delete</li>
+              </ul>
+            </div>
+            <div className="bg-gray-700 opacity-80 p-4 m-auto w-4/5 flex">
+              <ul className="w-2/3">
+                <li>Character Name</li>
+                <li>Character Class/Lvl</li>
+                <li>Character Race/Background</li>
+              </ul>
+              <ul className="w-1/3">
+                <li>Edit</li>
+                <li>Favorite</li>
+                <li>Delete</li>
+              </ul>
+            </div>
+            <div className="bg-gray-700 opacity-80 p-4 m-auto w-4/5 flex">
+              <ul className="w-2/3">
+                <li>Character Name</li>
+                <li>Character Class/Lvl</li>
+                <li>Character Race/Background</li>
+              </ul>
+              <ul className="w-1/3">
+                <li>Edit</li>
+                <li>Favorite</li>
+                <li>Delete</li>
+              </ul>
+            </div>
+            <div className="bg-gray-700 opacity-80 p-4 m-auto w-4/5 flex">
+              <ul className="w-2/3">
+                <li>Character Name</li>
+                <li>Character Class/Lvl</li>
+                <li>Character Race/Background</li>
+              </ul>
+              <ul className="w-1/3">
+                <li>Edit</li>
+                <li>Favorite</li>
+                <li>Delete</li>
+              </ul>
+            </div>
+            <div className="bg-gray-700 opacity-80 p-4 m-auto w-4/5 flex">
+              <ul className="w-2/3">
+                <li>Character Name</li>
+                <li>Character Class/Lvl</li>
+                <li>Character Race/Background</li>
+              </ul>
+              <ul className="w-1/3">
+                <li>Edit</li>
+                <li>Favorite</li>
+                <li>Delete</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2>Tools:</h2>
+          <div>
+            <ul>
+              <li>Random Build</li>
+              <li>Sort</li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
