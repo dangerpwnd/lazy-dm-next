1c556aa5a3a8d6fbb63bf4003e13b3eab03d5f77
Eric Dangerstone <reric2012@gmail.com>
2023-11-18 22:43:45 -0600

    Removed additional Navigation component

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,8 @@
 import '../styles/globals.css';
-import Navigation from '../components/navigation.jsx';
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
-      <Navigation />
       <Component {...pageProps} />
     </div>
   );
