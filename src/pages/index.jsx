84ba4952000ee98be396bbe50da7fe21d2cce05c
Eric Dangerstone <reric2012@gmail.com>
2023-11-18 22:44:48 -0600

    Removed unused component and class

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 import Navigation from '../components/navigation.jsx';
 
-import Navigation from '../components/navigation.jsx';
-import theme from '../../theme.js';
-
 const HomePage = () => {
   return (
     <div>
       <Navigation />
-      <h1 class="font-serif">Welcome to the Lazy DM Dashboard</h1>
+      <h1>Welcome to the Lazy DM Dashboard</h1>
     </div>
   );
 };
