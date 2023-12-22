a1c670eb350504c142fb63a3b41147ae6ca7b5ec
dangerpwnd <eric@dangerstone.com>
2023-12-22 16:06:22 -0600

    Added h1 title, destructred title prop

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -2,54 +2,57 @@ import { useState } from 'react';
 
 import Link from 'next/link';
 
-const Navigation = () => {
+const Navigation = ({ title }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => setIsOpen(!isOpen);
 
   return (
-    <nav
-      className={`circular-nav ${isOpen ? 'open' : ''}`}
-      onClick={toggleOpen}
-    >
-      <img
-        src="/assets/images/logo-dragon.png"
-        alt="D&D Dragon Icon"
-        className="logo"
-      />
-      <div className="links">
-        <Link
-          href="/characters"
-          className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
-        >
-          Characters
-        </Link>
-        <Link
-          href="/strongstart"
-          className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
-        >
-          Strong Start
-        </Link>
-        <Link
-          href="/scenes"
-          className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
-        >
-          Possible Scenes
-        </Link>
-        <Link
-          href="/secrets"
-          className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
-        >
-          Secrets & Clues
-        </Link>
-        <Link
-          href="/locations"
-          className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
-        >
-          Fantastic Locations
-        </Link>
-      </div>
-    </nav>
+    <div className="grid grid-cols-4">
+      <nav
+        className={`circular-nav ${isOpen ? 'open' : ''}`}
+        onClick={toggleOpen}
+      >
+        <img
+          src="/assets/images/logo-dragon.png"
+          alt="D&D Dragon Icon"
+          className="logo"
+        />
+        <div className="links">
+          <Link
+            href="/characters"
+            className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
+          >
+            Characters
+          </Link>
+          <Link
+            href="/strongstart"
+            className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
+          >
+            Strong Start
+          </Link>
+          <Link
+            href="/scenes"
+            className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
+          >
+            Possible Scenes
+          </Link>
+          <Link
+            href="/secrets"
+            className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
+          >
+            Secrets & Clues
+          </Link>
+          <Link
+            href="/locations"
+            className="shadow-md text-white bg-gray p-4 hover:bg-purple-700"
+          >
+            Fantastic Locations
+          </Link>
+        </div>
+      </nav>
+      <h1 className="col-span-2 text-center py-4">{title}</h1>
+    </div>
   );
 };
 
