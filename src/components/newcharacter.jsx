a3b3756e38eb888de08e84877ebcac0b1e079203
dangerpwnd <eric@dangerstone.com>
2024-03-29 15:23:11 -0500

    Created NewCharacter component

---
+++ src/components/newcharacter.jsx
@@ -0,0 +1,11 @@
+const NewCharacter = () => {
+  return (
+    <div className="flex justify-center">
+      <button className="text-3xl border-dashed border-t-2 border-l-2 transform transition-transform duration-500 hover:border-t-0  hover:border-l-0 hover:border-b-2 hover:border-r-2">
+        Add New Character
+      </button>
+    </div>
+  );
+};
+
+export default NewCharacter;
