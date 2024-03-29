8001193c6a747d539935cd725c161388c8e04f34
dangerpwnd <eric@dangerstone.com>
2024-03-29 15:37:16 -0500

    Updated margin layout, Added checkboxs to death saves

--- src/components/character.jsx
+++ src/components/character.jsx
@@ -1,18 +1,39 @@
 const Character = ({ name, cls, lvl, race, backgd, AC, HP }) => {
   return (
-    <div className="bg-gray-700 p-4 m-auto w-4/5 flex">
+    <div className="bg-gray-700 p-4 m-4 w-4/5 flex">
       <ul className="w-2/3 grid grid-cols-2">
         <li>
           <h3 className="bg-blue-300">{name}</h3>
         </li>
-        <li>Death saves: </li>
+        <li>
+          <ul>
+            <li>Death Saves:</li>
+            <li>
+              Success:{' '}
+              <span>
+                <input type="checkbox"></input>
+                <input type="checkbox"></input>
+                <input type="checkbox"></input>
+              </span>
+            </li>
+            <li>
+              Failures:{' '}
+              <span>
+                <input type="checkbox"></input>
+                <input type="checkbox"></input>
+                <input type="checkbox"></input>
+              </span>
+            </li>
+          </ul>
+        </li>
         <li>
           {cls} {lvl}
         </li>
+
+        <li>AC:{AC}</li>
         <li>
           {race} {backgd}
         </li>
-        <li>AC:{AC}</li>
         <li>HP:{HP}</li>
       </ul>
       <ul className="w-1/3">
